6112f85f9e5c44269fe46547b92714d0d76b958f
Jádson Alves <80630862+Jadson-Alves@users.noreply.github.com>
2021-06-16 14:59:23 -0300

    Add files via upload

---
+++ Instalar Azure Biceps e extensão VSCode.ps1
@@ -0,0 +1,22 @@
+﻿# Crie a pasta de instalação
+$installPath = "$env:USERPROFILE\.bicep"
+$installDir = New-Item -ItemType Directory -Path $installPath -Force
+$installDir.Attributes += 'Hidden'
+
+# Busque o binário Bicep CLI mais recente
+(New-Object Net.WebClient).DownloadFile("https://github.com/Azure/bicep/releases/latest/download/bicep-win-x64.exe", "$installPath\bicep.exe")
+
+# Adicione bicep ao seu PATH
+$currentPath = (Get-Item -path "HKCU:\Environment" ).GetValue('Path', '', 'DoNotExpandEnvironmentNames')
+if (-not $currentPath.Contains("%USERPROFILE%\.bicep")) { setx PATH ($currentPath + ";%USERPROFILE%\.bicep") }
+if (-not $env:path.Contains($installPath)) { $env:path += ";$installPath" }
+
+# Verifique se você agora pode acessar o comando 'bicep'
+bicep --help
+
+# Busque a extensão Bicep VSCode mais recente
+$vsixPath = "$env:TEMP\vscode-bicep.vsix"
+(New-Object Net.WebClient).DownloadFile("https://github.com/Azure/bicep/releases/latest/download/vscode-bicep.vsix", $vsixPath)
+
+# Instale a extensão
+code --install-extension $vsixPath
